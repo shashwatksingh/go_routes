345b603d5be47684385a0a70e2999a6a5570ec6b
shashwatksingh <shashwatksingh.27@gmail.com>
2026-03-30 21:26:25 +0100

    add home route

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
+
+func getEvents(context *gin.Context)  {
+	context.JSON(http.StatusOK, gin.H{"nessage": "You are here!"})
+}
 
 func main()  {
 	server := gin.Default()
 
-	server.GET("/", )
+	server.GET("/", getEvents)
 
 	server.Run(":4002")
 }
