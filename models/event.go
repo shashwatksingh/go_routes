fddfdca4d6eed58dda563d629ac12afbf9ab2486
shashwatksingh <shashwatksingh.27@gmail.com>
2026-03-30 21:45:12 +0100

    refining home route; adding events route

--- models/event.go
+++ models/event.go
@@ -3,16 +3,20 @@ package models
 import "time"
 
 type Event struct {
-	ID int
-	Name int
-	Decription int
-	Location string
-	DateTime time.Time
-	UserID int
+	ID int `json:"id"`
+	Name int `json:"name"`
+	Decription int `json:"description"`
+	Location string `json:"location"`
+	DateTime time.Time `json:"date_time"`
+	UserID int `json:"user_id"`
 }
 
 var events = []Event{}
 
 func (e Event) Save() {
 	events = append(events, e)
+}
+
+func GetAllEvents() []Event{
+	return events
 }
